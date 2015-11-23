e862607e9b9a66c100253edbc9b9563529aff843
androidthiago <thiagometal.fne@gmail.com>
2015-11-23 19:26:07 -0400

    Atualização do esqueci minha senha.

--- common/mail/passwordResetToken-html.php
+++ common/mail/passwordResetToken-html.php
@@ -7,9 +7,16 @@ use yii\helpers\Html;
 $resetLink = Yii::$app->urlManager->createAbsoluteUrl(['site/reset-password', 'token' => $user->password_reset_token]);
 ?>
 <div class="password-reset">
-    <p>Hello <?= Html::encode($user->username) ?>,</p>
+    <p>Olá <?= Html::encode($user->username) ?>,</p>
 
-    <p>Follow the link below to reset your password:</p>
+    <p>Um lembrete para a senha da sua conta no SGF-UFAM foi solicitado.</p>
+
+    <p>Você pode acessar sua conta agora clicando no link abaixo ou copiando e
+        colando-o no seu navegador:</p>
 
     <p><?= Html::a(Html::encode($resetLink), $resetLink) ?></p>
+
+    <p>Este link pode ser usado uma única vez para entrar no site e vai levar você
+        para uma página onde você pode trocar a sua senha.</p>
+
 </div>
--- common/mail/passwordResetToken-text.php
+++ common/mail/passwordResetToken-text.php
@@ -5,8 +5,14 @@
 
 $resetLink = Yii::$app->urlManager->createAbsoluteUrl(['site/reset-password', 'token' => $user->password_reset_token]);
 ?>
-Hello <?= $user->username ?>,
+Olá <?= $user->username ?>,
 
-Follow the link below to reset your password:
+Um lembrete para a senha da sua conta no SGF-UFAM foi solicitado.
+
+Você pode acessar sua conta agora clicando no link abaixo ou copiando e
+colando-o no seu navegador:
 
 <?= $resetLink ?>
+
+Este link pode ser usado uma única vez para entrar no site e vai levar você
+para uma página onde você pode trocar a sua senha.
