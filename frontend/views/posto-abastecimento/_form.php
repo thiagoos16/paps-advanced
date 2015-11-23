ff1f276e329792bf172b867b8223ae258c4f44fd
albertosql <albertosql@hotmail.com>
2015-11-23 18:53:35 -0400

    Alterando o create

--- frontend/views/posto-abastecimento/_form.php
+++ frontend/views/posto-abastecimento/_form.php
@@ -4,22 +4,27 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
-/* @var $model app\models\PostoAbastecimento */
+/* @var $model frontend\models\PostoAbastecimento */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
-<div class="posto-abastecimento-form">
+<div class="box box-primary">
+    <div class="box-header with-border">
+        <div class="posto-abastecimento-form">
 
-    <?php $form = ActiveForm::begin(); ?>
+            <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'nome')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'nome')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'endereco')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'endereco')->textInput(['maxlength' => true]) ?>
 
-    <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Novo' : 'Atualizar', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+            <div class="form-group">
+                <?= Html::submitButton($model->isNewRecord ? 'Novo' : 'Atualizar', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+            </div>
+
+            <?php ActiveForm::end(); ?>
+
+        </div>
     </div>
-
-    <?php ActiveForm::end(); ?>
 
 </div>
