24bc48fe35d5ddadcd5a1e0a3b253e02b3941751
androidthiago <thiagometal.fne@gmail.com>
2015-11-25 11:29:38 -0400

    Inclusão do advanced/commom/config/main-local.php no Git.

---
+++ common/config/main-local.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=advanced_yii2',
+            'username' => 'root',
+            'password' => 'root777',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',
+                'username' => 'sgfufam@gmail.com',
+                'password' => 'paps2015',
+                'port' => '465',
+                'encryption' => 'ssl',
+            ],
+            'useFileTransport' => false,
+        ],
+    ],
+];
