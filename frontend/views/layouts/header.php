f227f7fb4f42dd0ae19c77cee4501bf132101ed5
Ingrid do Nascimento Mendes <ingrid.mendes.si@gmail.com>
2015-12-10 19:54:59 -0300

    header e notificações de alertas de solicitações

--- frontend/views/layouts/header.php
+++ frontend/views/layouts/header.php
@@ -23,29 +23,100 @@ use frontend\models\Motorista;
           <div class="navbar-custom-menu">
             <ul class="nav navbar-nav">
               <!-- Messages: style can be found in dropdown.less-->
-              <li class="dropdown messages-menu">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                  <i class="fa fa-envelope-o"></i>
-                  <span class="label label-success">4</span>
-                </a>
-                <ul class="dropdown-menu">
-                  <li class="header">You have 4 messages</li>
-                  <li>
-                    <!-- inner menu: contains the actual data -->
-                    <ul class="menu">
-                      <li><!-- start message -->
-                        <a href="#">
-                          <div class="pull-left">
-                            <img src="<?=$baseUrl?>/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-                          </div>
-                          <h4>
-                            Support Team
-                            <small><i class="fa fa-clock-o"></i> 5 mins</small>
-                          </h4>
-                          <p>Why not buy a new awesome theme?</p>
-                        </a>
-                      </li><!-- end message -->
-                      <li>
+                  <?php
+
+                  global $num;
+                  $num=0;
+
+                  $connection = \Yii::$app->db;
+                  $model = $connection->createCommand("SELECT * FROM solicitacao WHERE solicitacao.status='Em análise'");
+                  $solicitacoes=$model->queryAll();
+
+                  foreach ($solicitacoes as $reg):
+                      $num++;
+                  endforeach;
+
+                  if (Usuario::findOne(Yii::$app->getUser()->id)->id_departamento == "1") {
+                      echo " <li class='dropdown messages-menu'>";
+                      echo"<a href='#' class='dropdown-toggle' data-toggle='dropdown'>";
+                      echo"<i class='fa fa-envelope-o'></i>";
+                      echo "<span class='label label-success'>$num</span>";
+                  }
+
+
+
+                        echo "</a>";
+                        echo "<ul class='dropdown-menu'>";
+
+                        if($num==0) {
+                            echo "<li class='header'>Não existem novas mensagens</li>";
+                        }
+                        elseif ($num==1) {
+                            echo "<li class='header'>Você possui $num mensagem</li>";
+                        }
+                        else {
+                            echo "<li class='header'>Você possui $num mensagens</li>";
+                        }
+
+                        echo "<li>";
+                        // inner menu: contains the actual data
+                        echo "<ul class='menu'>";
+
+                        $hoje = date("d/m/Y");
+
+                        // Cria uma função que retorna o timestamp de uma data no formato DD/MM/AAAA
+                        function geraTimestamp($data) {
+                            $partes = explode('/', $data);
+                            return mktime(0, 0, 0, $partes[1], $partes[0], $partes[2]);
+                        }
+
+                        foreach ($solicitacoes as $reg):
+                            $id_usuario = "{$reg['id_usuario']}";
+                            $data_lancamento = "{$reg['data_lancamento']}";
+                            $lancamento=date('d/m/Y',strtotime($data_lancamento));
+                            $id = "{$reg['id']}";
+
+                            $time_inicial = geraTimestamp($lancamento);
+                            $time_final = geraTimestamp($hoje);
+
+                            $diferenca = $time_final - $time_inicial;
+
+                            $dias = (int)floor( $diferenca / (60 * 60 * 24));
+
+                            $model = $connection->createCommand("SELECT * FROM user WHERE user.id='$id_usuario'");
+                            $usuario = $model->queryAll();
+                            foreach ($usuario as $reg):
+                                $nome = "{$reg['nome']}";
+                                $id_departamento = "{$reg['id_departamento']}";
+                            endforeach;
+
+                            $model = $connection->createCommand("SELECT * FROM departamento WHERE id='$id_departamento'");
+                            $dep = $model->queryAll();
+
+                            foreach($dep as $reg):
+                                $nome_dep =  "{$reg['nome']}";
+                            endforeach;
+
+                            echo "<li>"; //<!-- start message -->
+                            echo "<a href='index.php?r=solicitacao%2Fview&id=$id'>";
+                            echo "<h4>";
+                            echo "$nome_dep";
+                            if ($dias==0) {
+                                echo "<small><i class='fa fa-clock-o'></i> hoje</small>";
+                            }
+                            elseif ($dias==1) {
+                                echo "<small><i class='fa fa-clock-o'></i> ontem </small>";
+                            }
+                            else {
+                                echo "<small><i class='fa fa-clock-o'></i> $dias dias</small>";
+                            }
+                            echo "</h4>";
+                            echo "<p>Uma nova solicitação de <br> $nome </p>";
+                            echo "</a>";
+                            echo "</li>"; //<!-- end message -->
+                        endforeach;
+                    ?>
+                        <!--<li>
                         <a href="#">
                           <div class="pull-left">
                             <img src="<?=$baseUrl?>/dist/img/user3-128x128.jpg" class="img-circle" alt="User Image">
@@ -56,8 +127,8 @@ use frontend\models\Motorista;
                           </h4>
                           <p>Why not buy a new awesome theme?</p>
                         </a>
-                      </li>
-                      <li>
+                      </li> -->
+                        <!--<li>
                         <a href="#">
                           <div class="pull-left">
                             <img src="<?=$baseUrl?>/dist/img/user4-128x128.jpg" class="img-circle" alt="User Image">
@@ -68,8 +139,8 @@ use frontend\models\Motorista;
                           </h4>
                           <p>Why not buy a new awesome theme?</p>
                         </a>
-                      </li>
-                      <li>
+                      </li>-->
+                        <!-- <li>
                         <a href="#">
                           <div class="pull-left">
                             <img src="<?=$baseUrl?>/dist/img/user3-128x128.jpg" class="img-circle" alt="User Image">
@@ -80,8 +151,8 @@ use frontend\models\Motorista;
                           </h4>
                           <p>Why not buy a new awesome theme?</p>
                         </a>
-                      </li>
-                      <li>
+                      </li> -->
+                        <!--<li>
                         <a href="#">
                           <div class="pull-left">
                             <img src="<?=$baseUrl?>/dist/img/user4-128x128.jpg" class="img-circle" alt="User Image">
@@ -92,17 +163,14 @@ use frontend\models\Motorista;
                           </h4>
                           <p>Why not buy a new awesome theme?</p>
                         </a>
-                      </li>
+                      </li>-->
                     </ul>
                   </li>
-                  <li class="footer"><a href="#">See All Messages</a></li>
-                </ul>
-              </li>
-              <!-- Notifications: style can be found in dropdown.less -->
-              <li class="dropdown notifications-menu">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                  <i class="fa fa-bell-o"></i>
-                    <?php
+                    <!-- <li class="footer"><a href="#">See All Messages</a></li> -->
+                   </ul>
+                 </li>
+                 <!-- Notifications: style can be found in dropdown.less -->
+                <?php
 
                     global $count;
                     $count=0;
@@ -122,12 +190,6 @@ use frontend\models\Motorista;
                     $data_inicial = date("d/m/Y");
                     $data_final = date('d/m/Y', $timestamp);
 
-                    // Cria uma função que retorna o timestamp de uma data no formato DD/MM/AAAA
-                    function geraTimestamp($data) {
-                        $partes = explode('/', $data);
-                        return mktime(0, 0, 0, $partes[1], $partes[0], $partes[2]);
-                    }
-
                     foreach ($motoristas as $reg):
                         $count++;
                     endforeach;
@@ -136,13 +198,27 @@ use frontend\models\Motorista;
                         $count++;
                     endforeach;
 
-                    echo"<span class='label label-warning'>$count</span>";
+                    if(Usuario::findOne(Yii::$app->getUser()->id)->id_departamento == "1") {
+                        echo "<li class='dropdown notifications-menu'>";
+                        echo "<a href='#' class='dropdown-toggle' data-toggle='dropdown'>";
+                        echo "<i class='fa fa-bell-o'></i>";
+                        echo"<span class='label label-warning'>$count</span>";
+                    }
+
+
                     echo "</a>";
                     echo "<ul class='dropdown-menu'>";
 
 
-
-                  echo"<li class='header'>Você possui $count notificações</li>";
+                    if ($count==0) {
+                        echo"<li class='header'>Não existem novas notificações</li>";
+                    }
+                    elseif($count==1){
+                        echo"<li class='header'>Você possui $count notificação</li>";
+                    }
+                    else{
+                        echo"<li class='header'>Você possui $count notificações</li>";
+                    }
                   echo "<li>";
                     // inner menu: contains the actual data
                     echo "<ul class='menu'>";
@@ -166,7 +242,12 @@ use frontend\models\Motorista;
                             echo "<li>";
                             echo "<a href='index.php?r=motorista%2Fupdate&id=$cnh'>";
                             echo " <i class='fa fa-user text-yellow'></i> ";
-                            echo "A carteira do(a) motorista <br> <strong> $nome </strong> vencerá em $dias dias";
+                            if($dias==1) {
+                                echo "A carteira do(a) motorista <br> <strong> $nome </strong> vencerá em $dias dia";
+                            }
+                            else {
+                                echo "A carteira do(a) motorista <br> <strong> $nome </strong> vencerá em $dias dias";
+                            }
                             echo "</a>";
                             echo " </li>";
                         endforeach;
@@ -176,7 +257,7 @@ use frontend\models\Motorista;
                             $renavam = "{$reg['renavam']}";
                             echo "<li>";
                             echo "<a href='index.php?r=veiculo%2Fupdate&id=$renavam'>";
-                            echo " <i class='fa fa-car text-red'></i> ";
+                            echo " <i class='fa fa-car text-yellow'></i> ";
                             echo "O veículo de placa <br> <strong> $placa </strong> está em manutenção";
                             echo "</a>";
                             echo " </li>";
